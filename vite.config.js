1198afce610ed18c3cac37f385fba288948f02d3
shrutivtu <shrutisaxena1596@gmail.com>
2026-05-15 09:12:11 +0200

    add Tauri

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const isTauri = process.env.TAURI_ENV_PLATFORM != null;
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    strictPort: true,
+    watch: {
+      ignored: ['**/src-tauri/**'],
+    },
+  },
+  envPrefix: ['VITE_', 'TAURI_ENV_'],
 });
